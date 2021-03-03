086e02c7adac048d567445a91663bcb911478ec0
EvanKelemen <evan.kelemen@gmail.com>
2021-03-03 10:01:44 -0500

    Trimmed Code

--- ExtractingvaluesTEMP_LANDCOVER.R
+++ ExtractingvaluesTEMP_LANDCOVER.R
@@ -1,21 +1,22 @@
 #-------------------------------------------------------------------------------
 # Calculating Change in temperature through time
 #-------------------------------------------------------------------------------
-#https://gis.stackexchange.com/questions/29118/how-to-find-the-average-raster-value-of-an-area-defined-by-a-shapefile-using-r
-# $ indicates that the pattern is at the end of the file name
-#rlist=list.files(getwd(), pattern="img$", full.names=TRUE) 
-
-rlist=list.files("C:/Users/evank/Documents/ArcGIS/Projects/Temperature/Temperature Variable CRU/Previous Summer", 
-                 pattern=".tif$", full.names=TRUE) 
-
-r <- stack(rlist)   
-
 # Add required libraries
 require(raster)
 require(sp)
 require(rgdal)
+library(lme4)
 
-# Set working directory, raster, in and out shapefiles
+#https://gis.stackexchange.com/questions/29118/how-to-find-the-average-raster-value-of-an-area-defined-by-a-shapefile-using-r
+# $ indicates that the pattern is at the end of the file name
+#rlist=list.files(getwd(), pattern="img$", full.names=TRUE) 
+
+#>>>>>>> Temperature Data
+# Compile the files
+rlist=list.files("C:/Users/evank/Documents/ArcGIS/Projects/Temperature/Temperature Variable CRU/Previous Summer", 
+                 pattern=".tif$", full.names=TRUE) 
+
+r <- stack(rlist)   
 
 # Create a polygon 
 # A list of all lat and long
@@ -29,106 +30,148 @@ xym
 p = Polygon(xym)
 ps = Polygons(list(p),1)
 sdata = SpatialPolygons(list(ps))
-plot(sdata)
-
+#plot(sdata)
 
 # Extract raster values to list object
 r.vals <- extract(r, sdata)
 
-
 r.vals <- r.vals[[1]]
+#Removes NAs
 r.vals <- na.omit(r.vals)
 
 # Use list apply to calculate mean for each polygon
 r.mean <- apply(r.vals, MARGIN = 2,  FUN=mean)
 
-length(r.mean)
-
+# Make a Data Frame of the Data
+temp.through.time <- data.frame(summer.temp = r.mean, year= 1901:2018)
 # Analyze the data
-temp.through.time <- data.frame(ave.temp = r.mean, year= 1901:2018)
-temp.through.time.lm <- lm(ave.temp ~ year, data = temp.through.time)
+temp.through.time.lm <- lm(summer.temp ~ year, data = temp.through.time)
 summary(temp.through.time.lm)
 
 
 
-# Land USe 
-us.year <- c(1992) #, 1982, 1992, 2002, 2012)
-result <- vector("character", 1)
-for (i in 1:1){
+
+#>>>>> Precipiatation Data
+# Make a list of all the precipiation data
+rlist=list.files("C:/Users/evank/Documents/ArcGIS/Projects/Temperature/Temperature Variable CRU/Previous Precip", 
+                 pattern=".tif$", full.names=TRUE) 
+# Stack the files 
+r <- stack(rlist)   
+
+
+# Extract raster values to list object using sdata from above
+r.vals <- extract(r, sdata)
+
+r.vals <- r.vals[[1]]
+# Removes NAs
+r.vals <- na.omit(r.vals)
+
+# Use list apply to calculate mean for each polygon
+r.mean <- apply(r.vals, MARGIN = 2,  FUN=mean)
+
+#Make Data Frame
+precip.through.time <- data.frame(summer.precip = r.mean, year= 1901:2018)
+# Analyze the data
+precip.through.time.lm <- lm(summer.precip ~ year, data = precip.through.time)
+summary(precip.through.time.lm)
+
+
+
+
+
+
+#>>>>>>> Land USe 
+# Originally wanted to use a for loop to load in ever raster. So large, had to 
+# Load in one at a time
+
+us.year <- c(1974, 1982, 1992, 2002, 2012)
+r.us <- stack()
+for (i in 1:5){
   filename <- list.files(
     "C:/Users/evank/Documents/ArcGIS/Projects/Temperature/",
     pattern=paste0("lu", us.year[i]), full.names=TRUE)
-  result[i] <- filename[1]
+  temp <- raster(filename)
+  # Crop the dimensions of the raster to equal that of sampling area
+  temp <- crop(temp, sdata)
+  r.us <- stack(temp, r.us)
 }
 
-
-
-
-ca.year <- c( 2000) 
-result.ca <- vector("character", 1)
-for (i in 1:1){
+ca.year <- c(1990, 2000, 2010)
+r.ca <- list()
+for (i in 1:3){
   filename <- list.files(paste0(
     "C:/Users/evank/Documents/ArcGIS/Projects/Temperature/"),
     pattern=paste0("ca", ca.year[i], ".tif$"), full.names=TRUE) 
-  result.ca[i] <- filename
+  temp <- raster(filename)
+  # Crop the dimensions of the raster to equal that of sampling area
+  temp <- crop(temp, sdata)
+  r.ca <- list(temp, r.ca)
 }
 
-# These stacks are to large to work with
-r.us <- stack(result)
-
-r.ca <- stack(result.ca)
-
-# Check if the raster and the sampling area overlap
-plot(r.ca)
-plot(sdata, add = T)
-
-# Crop the dimensions of the raster to equal that of sampling area
-r.us <- crop(r.us, sdata)
-
-
-
-#-----Scratch-----
-# Tried changing the projection in R but "no enough memory" so changed projection in ArcGIS
-#rasterOptions(memfrac=.3)
-#r.ca <- projectRaster(r.ca,r, filename="r.ca", overwrite=T)
-# Make a raster to crop the raster down to work with
-#sdata.raster <- raster(sdata)
-#crs(sdata.raster) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
-
-# Potential way to extract information if the rasters were smaller
-#r.vals <- zonal(r.ca, sdata, 'sum')
-#-----End Scratch ----
-
-# Calculate the total number of cells
-number.of.cells <-ncell(r.us)
 
 # Calculates the frequency of different unique cell types
-land.use.frequncy<- freq(r.us)
+land.use.frequncy<- freq(r.ca)
+frequncy.list.ca <- lapply(r.ca, freq)
+land.use.frequncy.ca <- merge(frequncy.list.ca[[1]], frequncy.list.ca[[2]], by='value', suffixes=c(2010, 2000), all=TRUE)
+land.use.frequncy.ca <- merge(land.use.frequncy.ca, frequncy.list.ca[[3]], by='value', suffixes=c("", 1990), all=TRUE)
 
 # For Canada  Urban 21, 25, Cropland 51
 # For US      urban 21, 22, 23, 24, 25, 26, 27, 31, 32, 33, Cropland 43
 
-# Analyze the data
+# Extract Land-use Variable from summary tables
+landuse_value <- function(year, landuse_variable, us=TRUE){
+  if (us){
+  land.use.frequncy[land.use.frequncy$value %in% landuse_variable, paste0("lu", year, "_050815")]
+  } else {
+    land.use.frequncy.ca[land.use.frequncy.ca$value %in% landuse_variable, paste0("count", year)]
+  }
+}
 
-# manually fill in the data.frame from ncell and freq data
-land.use.through.time <- data.frame(land.use.ag.raw = c(163336772, # 1974 99517599 - 44, 8905575 - 45 630904 - 41
-                                                        163336772, # 1982 99517599 - 44, 8905575 - 45 630904 - 41
-                                                        (64197588), # 1990
-                                                        (64223227), # 2000
-                                                        (63632703)), # 2010
-                                    land.use.urb.raw = c((5607707 + 4354866 + 2111712 + 1687609 + 3647106 + 10816265 + 6038739 + 5467794 + 16987900 + 169237), # 1974
-                                                         (5607707 + 4354866 + 2111712 + 1687609 + 3647106 + 10816265 + 6038739 + 5467794 + 16987900 + 169237), # 1982
-                                                         (9563390 + 7065820), #1990
-                                                         (12045687 + 7139952), #2000
-                                                         (13429206 + 7602544)),#2010
-                                    land.use.total = c(1381443000, # 1974
-                                                       1381443000, # 1982
-                                                       4167202410, #1990
-                                                       4663759107, #2000
-                                                       4167202410), #2010
+
+# Fill in the data.frame from freq data
+land.use.through.time <- data.frame(land.use.ag.raw = c(landuse_value(51, 1974), # 1974 
+                                                        landuse_value(51, 1982), # 1982 
+                                                        landuse_value(51, 1992), # 1992 
+                                                        landuse_value(51, 2002), # 2002 
+                                                        landuse_value(51, 2012), # 2012 
+                                                        landuse_value(43, 1990, us=FALSE), # 1990
+                                                        landuse_value(43, 2000, us=FALSE), # 2000
+                                                        landuse_value(43, 2010, us=FALSE)), # 2010
+                                    land.use.urb.raw = c(landuse_value(c(21, 22, 23, 24, 25, 26, 27, 31, 32, 33), 1974), # 1974
+                                                         landuse_value(c(21, 22, 23, 24, 25, 26, 27, 31, 32, 33), 1982), # 1982
+                                                         landuse_value(c(21, 22, 23, 24, 25, 26, 27, 31, 32, 33), 1992), # 1992
+                                                         landuse_value(c(21, 22, 23, 24, 25, 26, 27, 31, 32, 33), 2002), # 2002
+                                                         landuse_value(c(21, 22, 23, 24, 25, 26, 27, 31, 32, 33), 2012), # 2012
+                                                         landuse_value(c(21, 25,), 1990, us=FALSE), #1990
+                                                         landuse_value(c(21, 25,), 2000, us=FALSE), #2000
+                                                         landuse_value(c(21, 25,), 2010, us=FALSE)),#2010
+                                    land.use.total = c(landuse_value(NA, 1974), # 1974
+                                                       landuse_value(NA, 1982), # 1982
+                                                       landuse_value(NA, 1992), # 1992
+                                                       landuse_value(NA, 2002), # 2002
+                                                       landuse_value(NA, 2012), # 2012
+                                                       landuse_value(c(NA), 1990, us=FALSE), #1990
+                                                       landuse_value(c(NA), 2000, us=FALSE), #2000
+                                                       landuse_value(c(NA), 2010, us=FALSE)), #2010
                                     year = c(1974, 1982, 1992, 2002, 2012,
-                                             1990, 2000, 2010))
+                                             1990, 2000, 2010),
+                                    country = c(rep("US", 5),
+                                                rep("CA", 3)))
                                     
-temp.through.time.lm <- lm(ave.temp ~ year, data = temp.through.time)
-summary(temp.through.time.lm)
+# Calulate the precentage of agriculature and urbanization
+land.use.through.time$percent.ag <- land.use.through.time$land.use.ag.raw/
+  land.use.through.time$land.use.total *100
+
+land.use.through.time$percent.urb <- land.use.through.time$land.use.urb.raw/
+  land.use.through.time$land.use.total * 100
+
+
+# Analyze the data
+ag.through.time.lm <- lme(percent.ag ~ year, random = ~1|country, data = land.use.through.time)
+summary(ag.through.time.lm)
+
+urb.through.time.lm <- lme(percent.urb ~ year, random = ~1|country, data = land.use.through.time)
+summary(urb.through.time.lm)
+
+plot(percent.urb~year, data= land.use.through.time)
 
